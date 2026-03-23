0bc5eafcfb17b17d9e6c9983fff1b87324d822b1
Karmadura <danielhiramoto1@gmail.com>
2026-03-23 18:53:59 +0100

    Contributor modifies constructor of Date

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,7 +19,7 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-  private String notes; // New variable of partner
+  private String notes;
   /**
    * Retrieves the day component of this date.
    *
