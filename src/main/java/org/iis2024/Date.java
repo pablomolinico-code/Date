9531f3dd217830586ca61738a3ba7874b008dca4
Karmadura <danielhiramoto1@gmail.com>
2026-03-23 18:58:13 +0100

    Contributor modifies constructor of Date

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -55,11 +55,7 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int dia, int mes, int año) {
-    this.day = dia;
-    this.month = mes;
-    this.year = año;
-  }
+
   public Date(int tag, int monat, int jahr) {
     this.day = tag;
     this.month = monat;
